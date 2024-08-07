176aeefda7de49c2c734aa33e693dae5991b4747
Giovani da Silva Ramos <gi_1997@hotmail.com>
2024-08-07 19:45:17 -0300

    Tipos e Variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,21 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 26;
+        short ano = 2024;
+        int cep = 21070333;
+        long cpf = 98765432109L;
+        float pi = 3.14F;
+        double salario = 2500.33;
+        String meuNome = "Giovani Ramos";
+
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero =5;
+        numero = 10;
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
