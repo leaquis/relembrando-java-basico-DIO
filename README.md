00a77b7d3b7ecdbe59885e45ecf9838c85a994c7
Giovani da Silva Ramos <gi_1997@hotmail.com>
2024-08-07 18:08:19 -0300

    subindo os arquivos

---
+++ README.md
@@ -0,0 +1,2 @@
+# relembrando-java-basico-DIO
+Repositório com os exercícios de java básico do bootcamp da DIO, que eu estou fazendo para relembrar alguns conceitos da linguagem
---
+++ java-anatomia-de-classes/src/MinhaClasse.java
@@ -0,0 +1,14 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String primeiroNome = "Giovani";
+        String segundoNome = "Ramos";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+}
