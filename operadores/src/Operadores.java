3300e9a5aac8dae38c61aa34673ab4357ab98c7b
Giovani da Silva Ramos <gi_1997@hotmail.com>
2024-08-08 03:48:14 -0300

    inicio operadores

---
+++ operadores/src/Operadores.java
@@ -0,0 +1,21 @@
+public class Operadores {
+    public static void main(String[] args) throws Exception {
+        //igualdade
+        String nome = "Giovani";
+        int idade = 26;
+        double peso = 80.5;
+        char sexo = 'M';
+        boolean doadorOrgao = false;
+        
+        //Operações
+        double soma = 10.5 + 15.7;
+        int subtracao = 113 - 25;
+        int multiplicacao = 20 * 7;
+        int divisao = 15 / 3;
+        int modulo = 18% 3;
+        double resultado = (10 * 7) + (20/4);
+
+        //Concatenação
+        String nomeCompleto = "Giovani " + "Ramos";
+    }
+}
