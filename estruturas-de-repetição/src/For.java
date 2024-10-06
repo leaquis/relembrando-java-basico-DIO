ae0b3702dcdebac9b077f835a39c890abc66687a
Giovani da Silva Ramos <gi_1997@hotmail.com>
2024-10-06 16:13:47 -0300

    For

---
+++ estruturas-de-repetição/src/For.java
@@ -0,0 +1,9 @@
+public class For {
+    public static void main(String[] args) throws Exception {
+        for (int carneirinhos = 1; carneirinhos <= 20; carneirinhos++) {
+            System.out.println("Contando carneirinhos: " + carneirinhos);
+        }
+
+        System.out.println("Joãozinho dormiu!");
+    }
+}
